7436667f8510266d5b1df3d775e6f99e4a653d62
ben <koppe.development@gmail.com>
2026-04-20 04:25:10 -0400

    Add sql-prepare pre-commit hook

--- flake-parts/pre-commit.nix
+++ flake-parts/pre-commit.nix
@@ -8,6 +8,14 @@ _: {
         hooks = {
           nixfmt.enable = true;
           rustfmt.enable = true;
+
+          # sqlx check custom hook
+          sql-prepare = {
+            enable = true;
+            entry = "cargo sqlx prepare --workspace -- --all-targets";
+            # add `--check` to check only. Without it the file will be updated when the hook is run
+            pass_filenames = false;
+          };
         };
       };
     };
